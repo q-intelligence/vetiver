546df609c40e109a48029a7bc201ef50977346ca
Edwin <wittekasteeltje@gmail.com>
2025-02-14 20:49:47 +0100

    thirth

--- app.R
+++ app.R
@@ -1,109 +1,87 @@
 library(shiny)
-library(ggplot2)
-library(DBI)
-library(RSQLite)
-make_reactive_trigger <- function() {
-    rv <- reactiveValues(a = 0)
-    list(
-        depend = function() {
-            rv$a
-            invisible()
-        },
-        trigger = function() {
-            rv$a <- isolate(rv$a + 1)
-        }
-    )
-}
-DBTrigger <- make_reactive_trigger()
-con <- dbConnect(RSQLite::SQLite(), "allocations.sqlite")
-dbExecute(con, 
-          'CREATE TABLE Containers (Container int, Amount float, CHECK(Amount >= 0))')
-dbExecute(con, 'INSERT INTO Containers (Container, Amount) VALUES(1, 10)')
-dbExecute(con, 'INSERT INTO Containers (Container, Amount) VALUES(2, 20)')
-dbExecute(con, 'INSERT INTO Containers (Container, Amount) VALUES(3, 30)')
-dbExecute(con, 'INSERT INTO Containers (Container, Amount) VALUES(4, 40)')
-initContainers <- dbGetQuery(con, 'SELECT Container, Amount FROM Containers')
+library(dplyr)
+library(purrr)
+library(gapminder)
+library(highcharter)
+
 
 ui <- fluidPage(
-    
     sidebarLayout(
         sidebarPanel(
-            tags$h5('Total Resources: 100'),
-            selectInput('From', 'Transfer from:', 
-                        choices = initContainers[['Container']]),
-            selectInput('To', 'Transfer to:', 
-                        choices = initContainers[['Container']][-1]),
-            numericInput('Amount', 'Amount', min = 0, value = 0, 
-                         max = initContainers[['Amount']][1]),
-            actionButton('Submit', 'Submit')
+            titlePanel("R Shiny Highcharts"),
+            selectInput(
+                inputId = "inContinent",
+                label = "Continent:",
+                choices = unique(gapminder$continent),
+                selected = "Europe"
+            ),
+            selectInput(
+                inputId = "inYearMin",
+                label = "Start year:",
+                choices = unique(gapminder$year)[1:length(unique(gapminder$year)) - 1],
+                selected = min(gapminder$year)
+            ),
+            selectInput(
+                inputId = "inYearMax",
+                label = "End year:",
+                choices = unique(gapminder$year)[2:length(unique(gapminder$year))],
+                selected = max(gapminder$year)
+            ),
+            width = 3
         ),
         mainPanel(
-            plotOutput("AllocationPlot")
+            tags$h3("Latest stats:"),
+            tags$div(
+                tags$div(
+                    tags$p("#Countries"),
+                    textOutput(outputId = "outNCountries")
+                ),
+                tags$div(
+                    tags$p("Median life exp."),
+                    textOutput(outputId = "outMedLifeExp")
+                ),
+                tags$div(
+                    tags$p("Median population"),
+                    textOutput(outputId = "outMedPop")
+                ),
+                tags$div(
+                    tags$p("Median GDP"),
+                    textOutput(outputId = "outMedGDP")
+                )
+            ),
+            width = 9
         )
     )
 )
 
-server <- function(input, output, session) {
-    Allocations <- reactive({
-        DBTrigger$depend()
-        dbGetQuery(con, 'SELECT Container, Amount FROM Containers')
-    })
-    #  Transferring to self is redundant
-    observeEvent(input$From, {
-        updateSelectInput(
-            session, 
-            'To', 
-            choices = Allocations()[['Container']][
-                !Allocations()[['Container']] %in% input$From])
-    })
-    # Limit tranfer amount
-    # this will produce a popup warning but is not currently enforced 
-    # server side
-    # see shinyvalidate package if using bootstrap3
-    observeEvent(Allocations(), {
-        i <- Allocations()[['Container']] == input$From
-        updateNumericInput(
-            session, 'Amount',
-            max = Allocations()[['Amount']][i])
-    })
-    observeEvent(input$Submit, {
-        res <- tryCatch({
-            dbBegin(con)
-            # disallow using negative values
-            stopifnot(input$Amount > 0)
-            dbExecute(con,
-                      sqlInterpolate(con,
-                                     'UPDATE Containers 
-                                     SET Amount = Amount + ?TransferAmount
-                                     WHERE Container = ?ToContainer',
-                                     TransferAmount = input$Amount,
-                                     ToContainer = input$To)
+server <- function(input, output) {
+    data_cards <- reactive({
+        gapminder %>%
+            filter(
+                continent == input$inContinent,
+                year == max(year)
+            ) %>%
+            summarise(
+                nCountries = n_distinct(country),
+                medianLifeExp = median(lifeExp),
+                medianPopM = median(pop / 1e6),
+                medianGDP = median(gdpPercap)
             )
-            dbExecute(con,
-                      sqlInterpolate(con,
-                                     'UPDATE Containers 
-                                     SET Amount = Amount - ?TransferAmount
-                                     WHERE Container = ?FromContainer',
-                                     TransferAmount = input$Amount,
-                                     FromContainer = input$From)
-            )
-            dbCommit(con)
-        }, error = function(e) {
-            dbRollback(con)
-            e
-        })
-        if ( inherits(res, 'error') ) {
-            showNotification(res$message)
-        }
-        DBTrigger$trigger()
     })
-    # Show Latest Allocations
-    output$AllocationPlot <- renderPlot({
-        ggplot(Allocations()) +
-            geom_col(aes(Container, Amount)) +
-            labs(title = sprintf('Total Allocated: %d', 
-                                 sum(Allocations()[['Amount']]))) +
-            theme_bw()
+    
+    output$outNCountries <- renderText({
+        data_cards()$nCountries
+    })
+    output$outMedLifeExp <- renderText({
+        paste(round(data_cards()$medianLifeExp, 1), "years")
+    })
+    output$outMedPop <- renderText({
+        paste0(round(data_cards()$medianPopM, 2), "M")
+    })
+    output$outMedGDP <- renderText({
+        paste0("$", round(data_cards()$medianGDP, 2))
     })
 }
+
+
 shinyApp(ui = ui, server = server)
